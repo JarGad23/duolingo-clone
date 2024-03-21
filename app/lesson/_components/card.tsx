eb677f7303cf5514f776415893fd890bf44f9370
JarGad23 <j.gad@m8b.pl>
2024-03-21 13:29:26 +0100

    Added quiz footer

--- app/lesson/_components/card.tsx
+++ app/lesson/_components/card.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
+import { useCallback } from "react";
 import { challenges } from "@/db/schema";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
+import { useAudio, useKey } from "react-use";
 
 type Props = {
   id: number;
@@ -27,6 +29,17 @@ export const Card = ({
   type,
   disabled,
 }: Props) => {
+  const [audio, _, controls] = useAudio({ src: audioSrc || "" });
+
+  const handleClick = useCallback(() => {
+    if (disabled) return;
+
+    controls.play();
+    onClick();
+  }, [disabled, onClick, controls]);
+
+  useKey(shortcut, handleClick, {}, [handleClick]);
+
   return (
     <div
       className={cn(
@@ -41,8 +54,9 @@ export const Card = ({
         disabled && "pointer-events:none hover:bg-white",
         type === "ASSIST" && "lg:p-3 w-full"
       )}
-      onClick={() => {}}
+      onClick={handleClick}
     >
+      {audio}
       {imageSrc && (
         <div className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full">
           <Image src={imageSrc} alt={text} fill />
