8f6554af96acdc98b8d555879b9d769c4b3ed25a
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2024-03-21 13:30:17 +0100

    Feature 17: Challenge quiz footer (#17)
    * Pacakge json changes
    
    * Added AI woices
    
    * Added quiz footer

--- app/lesson/_components/quiz.tsx
+++ app/lesson/_components/quiz.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { Header } from "./header";
 import { QuestionBubble } from "./question-bubble";
 import { Challenge } from "./challenge";
+import { Footer } from "./footer";
 
 type Props = {
   initialLessonId: number;
@@ -35,6 +36,15 @@ export const Quiz = ({
     return uncompltedIndex === -1 ? 0 : uncompltedIndex;
   });
 
+  const [selectedOption, setSelectedOption] = useState<number>();
+  const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+
+  const onSelect = (id: number) => {
+    if (status !== "none") return;
+
+    setSelectedOption(id);
+  };
+
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions ?? [];
 
@@ -62,9 +72,9 @@ export const Quiz = ({
               )}
               <Challenge
                 options={options}
-                onSelect={() => {}}
-                status="none"
-                selectedOption={undefined}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
                 disabled={false}
                 type={challenge.type}
               />
@@ -72,6 +82,7 @@ export const Quiz = ({
           </div>
         </div>
       </div>
+      <Footer disabled={!selectedOption} status={status} onCheck={() => {}} />
     </>
   );
 };
