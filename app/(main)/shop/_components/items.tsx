e41f35f27583c587d51920c517609da1f3d839f8
JarGad23 <j.gad@m8b.pl>
2024-03-24 21:08:47 +0100

    Shop page changes for subsciption and lib for url

--- app/(main)/shop/_components/items.tsx
+++ app/(main)/shop/_components/items.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { refillHearts } from "@/actions/user-progress";
+import { createStripeUrl } from "@/actions/user-subscription";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useTransition } from "react";
@@ -27,6 +28,22 @@ export const Items = ({ hasActiveSubscription, hearts, points }: Props) => {
     });
   };
 
+  const onUpgrade = () => {
+    if (pending || hasActiveSubscription) {
+      return;
+    }
+
+    startTransition(() => {
+      createStripeUrl()
+        .then((res) => {
+          if (res.data) {
+            window.location.href = res.data;
+          }
+        })
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
   return (
     <ul className="w-full">
       <div className="flex items-center w-full p-4 gap-x-4 border-t-2">
@@ -48,6 +65,17 @@ export const Items = ({ hasActiveSubscription, hearts, points }: Props) => {
               <p>{POINTS_TO_REFILL}</p>
             </div>
           )}
+        </Button>
+      </div>
+      <div className="flex items-center w-full p-4 pt-8 gap-x-4 border-t-2">
+        <Image src="/unlimited.svg" alt="Unlimited" width={60} height={60} />
+        <div className="flex-1">
+          <p className="text-neutral-700 text-base lg:text-xl font-bold">
+            Unlimited hearts
+          </p>
+        </div>
+        <Button disabled={pending || hasActiveSubscription} onClick={onUpgrade}>
+          {hasActiveSubscription ? "active" : "upgrade"}
         </Button>
       </div>
     </ul>
