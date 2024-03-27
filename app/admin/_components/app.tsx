a2c3a6cdd8fdecb512cf7533f252e16601f4f9a4
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2024-03-27 17:49:31 +0100

    Feature 28: Admin panel part 2 (#28)
    * Pacakge json changes
    
    * Api route added for other parts of app
    
    * Added UI in admin dashboard for other parts

--- app/admin/_components/app.tsx
+++ app/admin/_components/app.tsx
@@ -6,6 +6,22 @@ import { CourseList } from "../course/list";
 import { CourseCreate } from "../course/create";
 import { CourseEdit } from "../course/edit";
 
+import { UnitList } from "../unit/list";
+import { UnitCreate } from "../unit/create";
+import { UnitEdit } from "../unit/edit";
+
+import { LessonList } from "../lesson/list";
+import { LessonCreate } from "../lesson/create";
+import { LessonEdit } from "../lesson/edit";
+
+import { ChallengeList } from "../challenge/list";
+import { ChallengeCreate } from "../challenge/create";
+import { ChallengeEdit } from "../challenge/edit";
+
+import { ChallengeOptionList } from "../challengeOption/list";
+import { ChallengeOptionCreate } from "../challengeOption/create";
+import { ChallengeOptionEdit } from "../challengeOption/edit";
+
 const dataProvider = simpleRestProvider("/api");
 
 const App = () => {
@@ -17,6 +33,35 @@ const App = () => {
         list={CourseList}
         create={CourseCreate}
         edit={CourseEdit}
+      />
+      <Resource
+        name="units"
+        recordRepresentation="title"
+        list={UnitList}
+        create={UnitCreate}
+        edit={UnitEdit}
+      />
+      <Resource
+        name="lessons"
+        recordRepresentation="title"
+        list={LessonList}
+        create={LessonCreate}
+        edit={LessonEdit}
+      />
+      <Resource
+        name="challenges"
+        recordRepresentation="question"
+        list={ChallengeList}
+        create={ChallengeCreate}
+        edit={ChallengeEdit}
+      />
+      <Resource
+        name="challengeOptions"
+        list={ChallengeOptionList}
+        create={ChallengeOptionCreate}
+        edit={ChallengeOptionEdit}
+        recordRepresentation="text"
+        options={{ label: "Challenge Options" }}
       />
     </Admin>
   );
